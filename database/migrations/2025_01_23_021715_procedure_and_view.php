2b3c5d829bcad8df6e9626dcb5a8459b9332193f
LeninVasq <150496561+LeninVasq@users.noreply.github.com>
2025-01-22 20:51:50 -0600

    procedimientos y vistas

---
+++ database/migrations/2025_01_23_021715_procedure_and_view.php
@@ -0,0 +1,193 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //vistas
+
+        DB::statement("
+       CREATE
+    VIEW app_categoria_menu 
+    AS
+(SELECT
+  categoria_menu.id_categoria_menu AS id_categoria_menu,
+  categoria_menu.nombre            AS nombre,
+  categoria_menu.foto              AS foto,
+  SUM(menu.cantidad_platos)        AS cantidad_platos
+FROM (categoria_menu
+   JOIN menu
+     ON (categoria_menu.id_categoria_menu = menu.id_categoria))
+WHERE menu.cantidad_platos > 0
+GROUP BY categoria_menu.id_categoria_menu,categoria_menu.nombre
+,categoria_menu.foto);
+        ");
+
+
+        DB::statement("
+
+        CREATE
+        VIEW categoria_count_productos 
+        AS
+    (SELECT
+      c.id_categoria_pro AS id_categoria_pro,
+      c.nombre_categoria AS nombre_categoria,
+      c.descripcion      AS descripcion,
+      c.foto             AS foto,
+      c.estado           AS estado,
+      c.created_at       AS created_at,
+      c.updated_at       AS updated_at,
+      COUNT(p.id_categoria_pro) AS cantidad_productos
+    FROM (categoria_pro c
+       LEFT JOIN productos p
+         ON (c.id_categoria_pro = p.id_categoria_pro))
+    GROUP BY c.id_categoria_pro,c.nombre_categoria,c.descripcion,c.estado,c.created_at,c.updated_at,c.foto
+    ORDER BY COUNT(p.id_categoria_pro)DESC);
+         ");
+
+
+         DB::statement("
+
+         CREATE
+       VIEW grafica_productos_mas_utilizados 
+       AS
+   (SELECT
+     p.nombre AS nombre,
+     COUNT(0)     AS cantidad
+   FROM (receta_producto r
+      JOIN productos p
+        ON (r.id_producto = p.id_producto))
+   GROUP BY p.id_producto,p.nombre
+   ORDER BY COUNT(0)DESC
+   LIMIT 6);
+            ");
+
+
+            DB::statement("
+
+            CREATE
+            VIEW grafica_plato_mas_reservado 
+            AS
+        (SELECT
+          menu.nombre AS nombre,
+          COUNT(0)        AS cantidad
+        FROM (reservas_item
+           JOIN menu
+             ON (menu.id_menu = reservas_item.id_menu))
+        GROUP BY menu.id_menu,menu.nombre
+        ORDER BY COUNT(0)DESC
+        LIMIT 6);
+        
+               ");
+
+
+        
+
+
+         
+        //procedimientos
+
+        DB::statement("
+
+        CREATE PROCEDURE app_menu( IN id_categoria_param INT)
+    
+     BEGIN
+ 
+  SELECT
+         menu.id_menu         AS id_menu,
+         menu.nombre          AS nombre,
+         menu.precio          AS precio,
+         menu.cantidad_platos AS cantidad_platos
+     FROM menu
+     WHERE id_categoria = id_categoria_param AND cantidad_platos > 0;
+ END");
+
+
+ DB::statement("
+ CREATE PROCEDURE app_menu_img(     IN id_categoria_param INT
+)
+
+BEGIN
+SELECT
+        `menu`.`id_menu` AS `id_menu`,
+        `menu`.`img`     AS `img`
+    FROM `menu`
+    WHERE `id_categoria` = id_categoria_param AND cantidad_platos > 0;
+
+     END
+            ");
+
+
+                    DB::statement("
+                    CREATE PROCEDURE app_reservas(     IN usuario_id INT
+)
+
+BEGIN
+SELECT reservas_item.id_reserva_item,reservas.id_reservas, menu.nombre, menu.img, reservas_item.cantidad AS cantidad, reservas_item.precio
+FROM reservas
+INNER JOIN reservas_item ON reservas.id_reservas = reservas_item.id_reservas
+INNER JOIN menu ON reservas_item.id_menu = menu.id_menu
+INNER JOIN categoria_menu ON menu.id_categoria = categoria_menu.id_categoria_menu
+WHERE reservas.id_usuario = usuario_id AND reservas.fecha_entrega IS NULL
+;    
+END
+
+
+
+       
+                               ");
+
+
+
+
+
+                               DB::statement("
+                               CREATE PROCEDURE reservas_web(     
+       )
+          
+           BEGIN
+         SELECT 
+               reservas.id_reservas, 
+               reservas_item.id_menu, 
+               reservas_item.cantidad,
+                   reservas_item.precio,
+               users.img AS foto, 
+                   menu.nombre,
+               users.correo AS usuario_correo 
+           FROM 
+               reservas
+           INNER JOIN 
+               reservas_item ON reservas.id_reservas = reservas_item.id_reservas
+           INNER JOIN 
+               menu ON reservas_item.id_menu = menu.id_menu
+           INNER JOIN 
+               categoria_menu ON menu.id_categoria = categoria_menu.id_categoria_menu
+           INNER JOIN 
+               users ON reservas.id_usuario = users.id_usuario 
+           WHERE 
+                reservas.fecha_entrega IS NULL AND reservas.estado = 1;
+       END
+           
+                  
+                                          ");
+
+
+            }
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
